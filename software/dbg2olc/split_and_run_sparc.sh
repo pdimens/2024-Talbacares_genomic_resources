e91d611b0405772bb24cbea6ada9e083f244f78c
Pavel Dimens <pdimens@live.com>
2020-07-23 15:25:09 -0500

    simplify consensus

--- software/dbg2olc/split_and_run_sparc.sh
+++ software/dbg2olc/split_and_run_sparc.sh
@@ -28,26 +28,14 @@ python2 $(realpath ../software/dbg2olc/split_reads_by_backbone_openclose.py) -b 
 for file in $(find ${split_dir} -name "*.reads.fasta"); do
     chunk=`basename $file .reads.fasta`
 
-    cmd=""
     for iter in `seq 1 ${iterations}`; do
-
-        cmd="blasr --nproc 64 ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta --bestn 1 -m 5 --minMatch 19 --out ${split_dir}/${chunk}.mapped.m5"
-
-        cmd+="Sparc m ${split_dir}/${chunk}.mapped.m5 b ${split_dir}/${chunk}.fasta k 1 c 2 g 1 HQ_Prefix Contig boost 5 t 0.2 o ${split_dir}/${chunk}; "
-
-        if [ ${iter} -lt ${iterations} ]
-            then
-            #rename
-            cmd+="mv ${split_dir}/${chunk}.consensus.fasta ${split_dir}/${chunk}.fasta;"
-        fi
-
+        blasr --nproc 64 ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta --bestn 1 -m 5 --minMatch 19 --out ${split_dir}/${chunk}.mapped.m5
+        Sparc m ${split_dir}/${chunk}.mapped.m5 b ${split_dir}/${chunk}.fasta k 1 c 2 g 1 HQ_Prefix Contig boost 5 t 0.2 o ${split_dir}/${chunk}; 
     done
-    echo $cmd
-    eval $cmd
 
     #to save space
     cmd="rm ${split_dir}/${chunk}.mapped.m5 ${split_dir}/${chunk}.reads.fasta"
-    eval $cmd
+    #eval $cmd
 done
 
 echo "merging consensus reads"
